5b105a1f02ba853ee90d627cd9b14ebfc0e8a71b
justinlazaro-iselect <justin.lazaro@iselect.com.au>
2020-02-29 17:55:34 +0800

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,7 @@ import { Routes, RouterModule } from "@angular/router";
 import { AuthGuard } from "./auth-guard.service";
 import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
 import { ErrorPageComponent } from "./error-page/error-page.component";
+import { ServerResolver } from "./servers/server/server-resolver.service";
 
 const appRoutes: Routes = [
   { path: "", component: HomeComponent },
@@ -26,7 +27,11 @@ const appRoutes: Routes = [
     canActivateChild: [AuthGuard],
     component: ServersComponent,
     children: [
-      { path: ":id", component: ServerComponent },
+      {
+        path: ":id",
+        component: ServerComponent,
+        resolve: { server: ServerResolver }
+      },
       {
         path: ":id/edit",
         component: EditServerComponent,
@@ -50,7 +55,10 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [
+    // RouterModule.forRoot(appRoutes, { useHash: true }),
+    RouterModule.forRoot(appRoutes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
